39dab5555fc0a13464a5bc0a7922e71d150e7aba
Erick Klix <erick.klix@garmin.com>
2016-11-03 18:21:09 -0500

    updated styles for simpleWeatherApp.html

---
+++ weather.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    var Geo={};
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(success,error);
+    } else {
+        alert ("Geolocation is not supported");
+    }
+
+    function error(){
+        alert("We couldn't find you!");
+    }
+
+    function success(position){
+        Geo.lat = position.coords.latitude;
+        Geo.lng = position.coords.longitude;
+        getWeather();
+    }
+
+    function getWeather(){
+
+        var key = "c043d4759e8caed9";
+        if ($(state).val() && $(city).val()){
+            var Weather = "http://api.wunderground.com/api/" + key + "/forecast/geolookup/conditions/q/" + $(state).val() + "/" + $(city).val() + ".json";
+        }else if(Geo.lat && Geo.lng){
+            var Weather = "http://api.wunderground.com/api/" + key + "/forecast/geolookup/conditions/q/" + Geo.lat + "," + Geo.lng + ".json";    
+        } else {
+            alert ("Please enable your location or select a valid state and city.")
+        }
+
+        $.ajax({
+            url : Weather,
+            dataType : "jsonp",
+            success : function(data){
+                console.log(data);
+                var location = data['location']['city'];
+                var temp = data['current_observation']['temp_f'];
+                var img = data['current_observation']['icon_url'];
+                var desc = data['current_observation']['weather'];
+                var wind = data['current_observation']['wind_string'];
+
+                $('#location').html(location);
+                $('#temp').html(temp);
+                $('#desc').html(desc);
+                $('#wind').html(wind);
+            }
+        });
+
+    }
+    
+    $("#button").click(getWeather);
+    
+});
+
